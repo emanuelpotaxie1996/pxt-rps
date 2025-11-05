d807a66357559f087cfdded608b31cdbe7e32df1
emanuelpotaxie_1996 <emanuelaprendiending@gmail.com>
2025-11-05 20:29:08 +0100

    Refactor hands representation in game logic
    Refactor game logic to use numbers instead of enum for hands.

--- pps.ts
+++ pps.ts
@@ -1,155 +1,70 @@
 //% block="Pedra, paper, tisores" color=#C247FF
 //% weight=102 icon="\uf257"
 namespace pps {
-    export enum Hands {
-        //% block="Pedra"
-        Rock = images.createImage(".....\n.###.\n.###.\n.###.\n....."),
-        Paper = images.createImage(".###.\n.###.\n.###.\n.###.\n.###."),
-        //% block="Tisores"
-        Scissors = images.createImage("##..#\n##.#.\n..#..\n##.#.\n##..#")
-    }
-    /**
-     * Mostra una mà, que pot ser una pedra, un paper o unes tisores
-     * @param hand mà a mostrar
-     */
-    //% block="mostrar mà de %hand" weight=0
-    export function showHand(hand: Hands): void {
-        hand.showImage(0);
-    }
-    export function startRadio(group: number): number {
-        radio.setGroup(group);
-        let hand: Hands = Hands.Rock;
-        let turn: number = 1;
-        basic.forever(() => {
-            if (turn === 1) {
-                pps.showHand(hand);
-            }
-        });
-        input.onButtonPressed(Button.A, () => {
-            if (turn === 1) {
-                if (hand == Hands.Rock) {
-                    hand = Hands.Paper;
-                } else if (hand == Hands.Paper) {
-                    hand = Hands.Scissors;
-                } else {
-                    hand = Hands.Rock;
-                }
-            }
-        });
-    }
     /**
      * Inicia un joc de pedra, paper, tisores
      * Pedra guanya a tisores, paper guanya a pedra i tisores guanyen a paper.
      * Per seleccionar una mà, premi el botó A.
      * Per acceptar la mà seleccionada, premi el botó B.
      */
-    //% block="iniciar joc" weight=2
+    //% block="iniciar joc"
     export function start(): void {
-        let hand: Hands = Hands.Rock;
-        let cpu: Hands;
+        let hand: number = 0;
+        let cpu: number = 0;
         let wins: number = 0;
         let loses: number = 0;
-        let realWins: number = 0;
         let draws: number = 0;
         let turn: boolean = true;
-        basic.forever(() => {
-            if (turn && !(wins + loses === 6)) {
-                pps.showHand(hand);
+        function show(h: number): void {
+            if (h === 0) {
+                basic.showLeds(".....\n.###.\n.###.\n.###.\n.....");
+            } else if (h === 1) {
+                basic.showLeds(".###.\n.###.\n.###.\n.###.\n.###.");
+            } else {
+                basic.showIcon(IconNames.Scissors);
             }
-            if (wins + loses === 6) {
-                basic.clearScreen();
-                basic.pause(500);
-                if (wins >= loses) {
-                    basic.showString("HAS GUANYAT!", 100);
+        }
+        input.onButtonPressed(Button.A, (): void => {
+            if (turn && !(wins + loses + draws === 6)) {
+                if (hand < 2) {
+                    hand++;
                 } else {
-                    basic.showString("HAS PERDUT!", 100);
-                }
-                basic.clearScreen();
-                basic.pause(500);
-                basic.showString(`V: ${realWins}  D: ${loses}  E: ${draws}`, 100);
-                wins = 0;
-                loses = 0;
-                realWins = 0;
-                draws = 0;
-            }
-        });
-        input.onButtonPressed(Button.A, () => {
-            if (turn && !(wins + loses === 6)) {
-                if (hand === Hands.Rock) {
-                    hand = Hands.Paper;
-                } else if (hand === Hands.Paper) {
-                    hand = Hands.Scissors;
-                } else {
-                    hand = Hands.Rock;
+                    hand = 0;
                 }
             }
         });
-        input.onButtonPressed(Button.B, () => {
-            if (!(wins + loses === 6)) {
+        input.onButtonPressed(Button.B, (): void => {
+            if (!(wins + loses + draws === 6)) {
                 turn = false;
-                cpu = Object.values(Hands)[randint(0, 2];
-                music.play(music.stringPlayable("C D E F", 120), music.PlaybackMode.UntilDone);
-                basic.pause(700);
-                pps.showHand(cpu);
-                if (hand === Hands.Rock) {
-                    getWin(() => {
-                        drawFace();
-                        wins++;
-                        draws++;
-                    }, () => {
-                        basic.showIcon(IconNames.Sad);
-                        loses++
-                    }, () => {
-                        basic.showIcon(IconNames.Happy);
-                        wins++;
-                        realWins++;
-                    });
-                } else if (hand === Hands.Paper) {
-                    getWin(() => {
-                        basic.showIcon(IconNames.Happy);
-                            wins++;
-                        realWins++;
-                    }, () => {
-                        drawFace();
-                        wins++;
-                        draws++;
-                    }, () => {
-                        basic.showIcon(IconNames.Sad);
-                        loses++;
-                    });
+                music.play(music.stringPlayable("c D E F", 120), music.PlaybackMode.UntilDone);
+                show(cpu);
+                if (hand === cpu) {
+                    basic.showLeds(".....\n.#.#.\n.....\n#####\n.....");
+                    draws++;
+                } else if ((hand + 1) % 3 === cpu) {
+                    basic.showIcon(IconNames.Sad);
+                    loses++;
                 } else {
-                    getWin(() => {
-                        basic.showIcon(IconNames.Sad);
-                        loses++;
-                    }, () => {
-                        basic.showIcon(IconNames.Happy);
-                        wins++;
-                        realWins++;
-                    }, () => {
-                        drawFace();
-                        draws++;
-                    });
-                } 
+                    basic.showIcon(IconNames.Happy);
+                    wins++;
+                }
                 turn = true;
             }
         });
-        function getWin(rock: () => void, paper: () => void, scissors: () => void): void {
-            if (cpu === Hands.Rock) {
-                rock();
-            } else if (cpu === Hands.Paper) {
-                paper();
-            } else {
-                scissors();
+        basic.forever((): void => {
+            if (turn && !(wins + loses + draws === 6)) {
+                show(hand);
             }
-        }
-        function drawFace(): void {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . . . . .
-                # # # # #
-                . . . . .
-            `);
-        }
+            if (wins + loses + draws === 6) {
+                if (wins >= loses || draws >= loses) {
+                    basic.showString("HAS GUANYAT!", 100);
+                } else {
+                    basic.showString("HAS PERDUT", 100);
+                }
+                wins = 0;
+                loses = 0;
+                draws = 0;
+            }
+        });
     }
 }
